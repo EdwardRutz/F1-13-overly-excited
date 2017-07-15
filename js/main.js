9f3d8d49d72d97f29930bbeefd95db94c1ff295f
Edward R <edwardrz@users.noreply.github.com>
2017-07-15 15:06:12 -0500

    + js/

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log("main.js");
+
+// Create an array that contains the words in the sentence
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+
+	console.log("how many words in the sentence array?", sentence.length);
+
+//The addExcitement function should accept the array as the sole argument
+function addExcitement (sentence) {
+/*`for` loop that iterates over the array argument and outputs the words. */
+}
+	for (let i = 0; i < 12; i++){
+	console.log("i", sentence);
+	}
+
+
+// Invoke the function and pass in the array
+/* addExcitement(sentence) */
