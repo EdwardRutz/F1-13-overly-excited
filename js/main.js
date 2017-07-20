042ce24ab0cb761acfc88ac407a5ea9a0256bce3
Edward R <edwardrz@users.noreply.github.com>
2017-07-19 20:28:25 -0500

    added increment script.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 console.log("main.js");
 
-// Create an array that contains the words in the sentence
+/*** VARIABLES ***/
+let wordUp = "";
+let text1 = "";
+
+/*** FUNCTIONS & ARRAYS ***/
+
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
 	console.log("how many words in the sentence array?", sentence.length);
 
-//The addExcitement function should accept the array as the sole argument
-function addExcitement (sentence) {
-/*`for` loop that iterates over the array argument and outputs the words. */
-}
-	for (let i = 0; i < 12; i++){
-	console.log("i", sentence);
+//The addExcitement function accepting array
+function addExcitement (theWordArray) {
+	for (let i = 0; i < theWordArray.length; i++){
+	wordUp += " " + theWordArray[i];
+	 if ((i+1) % 3 === 0) {wordUp = wordUp + "!"} // Adds "!" to every third word.
+
+	console.log(wordUp);
+	document.write(`<br>`, wordUp);
+
 	}
 
 
-// Invoke the function and pass in the array
-/* addExcitement(sentence) */
+//Output to index.html
+	document.write("<br/>The sentence to increment is: ", sentence);
+	document.write("<br/>The number of words in the sentence are: ", sentence.length);
+}	
+
+//Invoke the function and pass in the array
+addExcitement(sentence);
+
+
+
+
