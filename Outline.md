252fcc9927ae933b57df011e7b88e01a89d8ca02
Edward R <edwardrz@users.noreply.github.com>
2017-07-20 19:11:32 -0500

    updated

---
+++ Outline.md
@@ -0,0 +1,29 @@
+OUTLINE
+
+## REQUIREMENTS:
+
+Create a function that stacks words in an a row with each progressive row incremented
+	- Each row is incremented by one word
+	``
+		The
+		The cow
+		The cow jumps
+	``
+## OUTLINE
+- Define variables
+- Create array of words
+- Create function
+	- Create a loop that prints one word 
+	- then increments and prints what was printed + 1.
+
+- If the current value of the [counter variable] can be evenly divided by 3 - using the JavaScript [remainder] operator - then add a single exclamation point (!) to the output.
+
+
+OUTPUT: in console then to index.html
+
+
+HINTS:
+// Create an array that contains the words in the sentence
+//The addExcitement function should accept the array as the sole argument
+/*`for` loop that iterates over the array argument and outputs the words. */
+// Invoke the function and pass in the array
--- js/main.js
+++ js/main.js
@@ -2,30 +2,29 @@ console.log("main.js");
 
 /*** VARIABLES ***/
 let wordUp = "";
-let text1 = "";
 
 /*** FUNCTIONS & ARRAYS ***/
 
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
-	console.log("how many words in the sentence array?", sentence.length);
+  console.log("how many words in the sentence array?", sentence.length);
 
 //The addExcitement function accepting array
 function addExcitement (theWordArray) {
-	for (let i = 0; i < theWordArray.length; i++){
-	wordUp += " " + theWordArray[i];
-	 if ((i+1) % 3 === 0) {wordUp = wordUp + "!"} // Adds "!" to every third word.
+  for (i = 0; i < theWordArray.length; i++){
+  wordUp += "" + theWordArray[i];
+   if ((i + 1) % 3 === 0) {wordUp = wordUp + "!";} // Adds "!" to every third word.
 
-	console.log(wordUp);
-	document.write(`<br>`, wordUp);
+  console.log(wordUp);
+  document.write(`<br>`, wordUp);
 
-	}
+  }
 
 
 //Output to index.html
-	document.write("<br/>The sentence to increment is: ", sentence);
-	document.write("<br/>The number of words in the sentence are: ", sentence.length);
-}	
+document.write("<br/>The sentence to increment is: ", sentence);
+document.write("<br/>The number of words in the sentence are: ", sentence.length);
+} 
 
 //Invoke the function and pass in the array
 addExcitement(sentence);
